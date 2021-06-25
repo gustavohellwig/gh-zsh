ccec605180d3ce60518ed55dd09f738e348d7a15
Gustavo Hellwig <7680647+gustavohellwig@users.noreply.github.com>
2021-06-25 14:37:15 +0300

    Update and rename zsh-theme to zsh-theme.sh

--- zsh-theme.sh
+++ zsh-theme.sh
@@ -10,11 +10,9 @@ else
     fi
 fi
 
-
 if mv -n ~/.zshrc ~/.zshrc-backup-$(date +"%Y-%m-%d"); then # backup .zshrc
     echo -e "Backed up the current .zshrc to .zshrc-backup-date\n"
 fi
-
 
 echo -e "Installing oh-my-zsh\n"
 if [ -d ~/.oh-my-zsh ]; then
@@ -24,7 +22,6 @@ else
 fi
 
 cp -f .zshrc ~/
-
 
 mkdir -p ~/.quickzsh       # external plugins, things, will be instlled in here
 
@@ -52,14 +49,13 @@ else
     git clone --depth=1 https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh/custom/plugins/zsh-history-substring-search
 fi
 
-
 # INSTALL FONTS
 
 echo -e "Installing Nerd Fonts version of Hack, Roboto Mono, DejaVu Sans Mono\n"
 
-wget -q --show-progress -N https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf -P ~/.fonts/
+# get -q --show-progress -N https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf -P ~/.fonts/
 wget -q --show-progress -N https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/RobotoMono/Regular/complete/Roboto%20Mono%20Nerd%20Font%20Complete.ttf -P ~/.fonts/
-wget -q --show-progress -N https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DejaVuSansMono/Regular/complete/DejaVu%20Sans%20Mono%20Nerd%20Font%20Complete.ttf -P ~/.fonts/
+# wget -q --show-progress -N https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DejaVuSansMono/Regular/complete/DejaVu%20Sans%20Mono%20Nerd%20Font%20Complete.ttf -P ~/.fonts/
 
 fc-cache -fv ~/.fonts
 
@@ -69,13 +65,13 @@ else
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 fi
 
-if [ -d ~/.quickzsh/fzf ]; then
-    cd ~/.quickzsh/fzf && git pull
-    ~/.quickzsh/fzf/install --all --key-bindings --completion --no-update-rc --64
-else
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.quickzsh/fzf
-    ~/.quickzsh/fzf/install --all --key-bindings --completion --no-update-rc --64
-fi
+# if [ -d ~/.quickzsh/fzf ]; then
+#     cd ~/.quickzsh/fzf && git pull
+#     ~/.quickzsh/fzf/install --all --key-bindings --completion --no-update-rc --64
+# else
+#     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.quickzsh/fzf
+#     ~/.quickzsh/fzf/install --all --key-bindings --completion --no-update-rc --64
+# fi
 
 if [ -d ~/.oh-my-zsh/custom/plugins/k ]; then
     cd ~/.oh-my-zsh/custom/plugins/k && git pull
@@ -83,17 +79,17 @@ else
     git clone --depth 1 https://github.com/supercrabtree/k ~/.oh-my-zsh/custom/plugins/k
 fi
 
-if [ -d ~/.quickzsh/marker ]; then
-    cd ~/.quickzsh/marker && git pull
-else
-    git clone --depth 1 https://github.com/pindexis/marker ~/.quickzsh/marker
-fi
+# if [ -d ~/.quickzsh/marker ]; then
+#     cd ~/.quickzsh/marker && git pull
+# else
+#     git clone --depth 1 https://github.com/pindexis/marker ~/.quickzsh/marker
+# fi
 
-if ~/.quickzsh/marker/install.py; then
-    echo -e "Installed Marker\n"
-else
-    echo -e "Marker Installation Had Issues\n"
-fi
+# if ~/.quickzsh/marker/install.py; then
+#     echo -e "Installed Marker\n"
+# else
+#     echo -e "Marker Installation Had Issues\n"
+# fi
 
 # if git clone --depth 1 https://github.com/todotxt/todo.txt-cli.git ~/.quickzsh/todo; then :
 # else
@@ -102,34 +98,29 @@ fi
 # mkdir ~/.quickzsh/todo/bin ; cp -f ~/.quickzsh/todo/todo.sh ~/.quickzsh/todo/bin/todo.sh # cp todo.sh to ./bin so only it is included in $PATH
 # #touch ~/.todo/config     # needs it, otherwise spits error , yeah a bug in todo
 # ln -s ~/.quickzsh/todo ~/.todo
-if [ ! -L ~/.quickzsh/todo/bin/todo.sh ]; then
-    echo -e "Installing todo.sh in ~/.quickzsh/todo\n"
-    mkdir -p ~/.quickzsh/todo/bin
-    wget -q --show-progress "https://github.com/todotxt/todo.txt-cli/releases/download/v2.11.0/todo.txt_cli-2.11.0.tar.gz" -P ~/.quickzsh/
-    tar xvf ~/.quickzsh/todo.txt_cli-2.11.0.tar.gz -C ~/.quickzsh/todo --strip 1 && rm ~/.quickzsh/todo.txt_cli-2.11.0.tar.gz
-    ln -s ~/.quickzsh/todo/todo.sh ~/.quickzsh/todo/bin/todo.sh     # so only .../bin is included in $PATH
-    ln -s ~/.quickzsh/todo/todo.cfg ~/.todo.cfg     # it expects it there or ~/todo.cfg or ~/.todo/config
-else
-    echo -e "todo.sh is already instlled in ~/.quickzsh/todo/bin/\n"
-fi
+# if [ ! -L ~/.quickzsh/todo/bin/todo.sh ]; then
+#     echo -e "Installing todo.sh in ~/.quickzsh/todo\n"
+#     mkdir -p ~/.quickzsh/todo/bin
+#     wget -q --show-progress "https://github.com/todotxt/todo.txt-cli/releases/download/v2.11.0/todo.txt_cli-2.11.0.tar.gz" -P ~/.quickzsh/
+#     tar xvf ~/.quickzsh/todo.txt_cli-2.11.0.tar.gz -C ~/.quickzsh/todo --strip 1 && rm ~/.quickzsh/todo.txt_cli-2.11.0.tar.gz
+#     ln -s ~/.quickzsh/todo/todo.sh ~/.quickzsh/todo/bin/todo.sh     # so only .../bin is included in $PATH
+#     ln -s ~/.quickzsh/todo/todo.cfg ~/.todo.cfg     # it expects it there or ~/todo.cfg or ~/.todo/config
+# else
+#     echo -e "todo.sh is already instlled in ~/.quickzsh/todo/bin/\n"
+# fi
 
-if [[ $1 == "--cp-hist" ]] || [[ $1 == "-c" ]]; then
-    echo -e "\nCopying bash_history to zsh_history\n"
-    if command -v python &>/dev/null; then
+echo -e "\nCopying bash_history to zsh_history\n"
+if command -v python &>/dev/null; then
+    wget -q --show-progress https://gist.githubusercontent.com/muendelezaji/c14722ab66b505a49861b8a74e52b274/raw/49f0fb7f661bdf794742257f58950d209dd6cb62/bash-to-zsh-hist.py
+    cat ~/.bash_history | python bash-to-zsh-hist.py >> ~/.zsh_history
+else
+    if command -v python3 &>/dev/null; then
         wget -q --show-progress https://gist.githubusercontent.com/muendelezaji/c14722ab66b505a49861b8a74e52b274/raw/49f0fb7f661bdf794742257f58950d209dd6cb62/bash-to-zsh-hist.py
-        cat ~/.bash_history | python bash-to-zsh-hist.py >> ~/.zsh_history
+        cat ~/.bash_history | python3 bash-to-zsh-hist.py >> ~/.zsh_history
     else
-        if command -v python3 &>/dev/null; then
-            wget -q --show-progress https://gist.githubusercontent.com/muendelezaji/c14722ab66b505a49861b8a74e52b274/raw/49f0fb7f661bdf794742257f58950d209dd6cb62/bash-to-zsh-hist.py
-            cat ~/.bash_history | python3 bash-to-zsh-hist.py >> ~/.zsh_history
-        else
-            echo "Python is not installed, can't copy bash_history to zsh_history\n"
-        fi
+        echo "Python is not installed, can't copy bash_history to zsh_history\n"
     fi
-else
-    echo -e "\nNot copying bash_history to zsh_history, as --cp-hist or -c is not supplied\n"
 fi
-
 
 # source ~/.zshrc
 echo -e "\nSudo access is needed to change default shell\n"
