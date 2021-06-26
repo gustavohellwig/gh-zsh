756edcbe42f5e4ced2ef188c492ecf2ad0007283
Gustavo Hellwig <7680647+gustavohellwig@users.noreply.github.com>
2021-06-26 15:18:49 +0300

    Update zsh-theme.sh

--- zsh-theme.sh
+++ zsh-theme.sh
@@ -35,3 +35,4 @@ wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/complet
 wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/history.zsh -P ~/.zsh/ &> /dev/null
 wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/key-bindings.zsh -P ~/.zsh/ &> /dev/null
 
+source ~/.zshrc
